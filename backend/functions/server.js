9c63748478b26531aaf6e4d8e832757c21bd1a19
ahmedohadi <ahadi@smu.edu>
2024-12-27 05:22:57 +0300

    Update backend

--- backend/functions/server.js
+++ backend/functions/server.js
@@ -8,14 +8,7 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server)
-// {
-  // cors: {
-  //   origin: "http://localhost:3000",
-  //   methods: ["GET", "POST"],
-  //   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
-  //   credentials: true,
-  // },
-// });
+
  
   app.use(
   cors({
